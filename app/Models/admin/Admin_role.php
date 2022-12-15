0c12ccca13d9aeeec75cf449b558b3b879210805
hakobyanhrayr <hrayr7711@gmail.com>
2022-12-15 18:36:45 +0400

    fix admin_role

--- app/Models/admin/Admin_role.php
+++ app/Models/admin/Admin_role.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Admin_role extends Model
 {
     use HasFactory;
-
+    protected $table = 'admin_role';
     protected $fillable = ['admin_id','role_id'];
 }
--- database/migrations/2022_12_14_122613_create_admin_roles_table.php
+++ database/migrations/2022_12_14_122613_create_admin_roles_table.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateAdminRolesTable extends Migration
 {
-    private const TABLE = 'admin_roles';
+    private const TABLE = 'admin_role';
 
     /**
      * Run the migrations.
